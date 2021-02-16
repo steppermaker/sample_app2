6519b4281781f3f1274f1f9a83e94adb966cf370
stepper <cwcpenbwb@gmail.com>
2021-02-17 01:09:37 +0900

    Add user-microposts

---
+++ test/integration/microposts_interface_test.rb
@@ -0,0 +1,46 @@
+require 'test_helper'
+
+class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:michael)
+  end
+
+  test "micropost interface" do
+    log_in_as(@user)
+    get root_path
+    assert_select 'div.pagination'
+    assert_select 'input[type=file]'
+
+    assert_no_difference 'Micropost.count' do
+      post microposts_path, params: { micropost: { content: "" } }
+    end
+    assert_select 'div#error_explanation'
+
+    assert_difference 'Micropost.count', 1 do
+      post microposts_path, params: { micropost: { content: "test" } }
+    end
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_match 'test', response.body
+
+    assert_select 'a', text: 'delete'
+    first_micropost = @user.microposts.paginate(page: 1).first
+    assert_difference 'Micropost.count', -1 do
+      delete micropost_path(first_micropost)
+    end
+    get user_path(users(:archer))
+    assert_select 'a', text: 'delete', count: 0
+  end
+
+  test "micropost sidebar count" do
+    # まだマイクロポストを投稿していないユーザー
+    other_user = users(:malory)
+    log_in_as(other_user)
+    get root_path
+    assert_match "0 microposts", response.body
+    other_user.microposts.create!(content: "A micropost")
+    get root_path
+    assert_match "A micropost", response.body
+  end
+end
